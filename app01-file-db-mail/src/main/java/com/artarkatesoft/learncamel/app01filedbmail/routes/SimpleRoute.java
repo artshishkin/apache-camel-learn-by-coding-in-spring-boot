0e139134d1668aa61312f71942ee4e2df5f14af9
admin <d.art.shishkin@gmail.com>
2020-10-13 23:22:48 +0300

    20 Running app with dev, stage and prod configuration

--- app01-file-db-mail/src/main/java/com/artarkatesoft/learncamel/app01filedbmail/routes/SimpleRoute.java
+++ app01-file-db-mail/src/main/java/com/artarkatesoft/learncamel/app01filedbmail/routes/SimpleRoute.java
@@ -1,15 +1,26 @@
 package com.artarkatesoft.learncamel.app01filedbmail.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleRoute extends RouteBuilder {
+
+    @Value("${valueMessage}")
+    String valueMess;
+
+    @Autowired
+    Environment environment;
+
     @Override
     public void configure() throws Exception {
-        from("timer:hello?period=10s")
-                .log("Timer invoked and body is `${body}`")
-                .pollEnrich("file:data/input?delete=true&readLock=none")
-                .to("file:data/output");
+        from("{{routeTimer}}")
+                .log("Timer invoked and evn. is `{{message}}`")
+                .pollEnrich("{{routeFrom}}")
+                .log("We have " + valueMess + " in " + environment.getProperty("envMessage") + " and copy File with content:\n${body}")
+                .to("{{routeTo1}}");
     }
 }
