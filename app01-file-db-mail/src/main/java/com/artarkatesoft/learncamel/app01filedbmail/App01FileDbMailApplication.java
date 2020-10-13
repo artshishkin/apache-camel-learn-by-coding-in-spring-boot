4e2e00cf7324250a31d737ff083aa8875fc593ea
Art <d.art.shishkin@gmail.com>
2020-10-13 18:31:14 +0300

    Initial commit

---
+++ app01-file-db-mail/src/main/java/com/artarkatesoft/learncamel/app01filedbmail/App01FileDbMailApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.learncamel.app01filedbmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App01FileDbMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App01FileDbMailApplication.class, args);
+	}
+
+}
---
+++ app01-file-db-mail/src/test/java/com/artarkatesoft/learncamel/app01filedbmail/App01FileDbMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.learncamel.app01filedbmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App01FileDbMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
