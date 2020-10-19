72c786485b90f6aaa8f2f15af8d742f6382bc2d7
Art <d.art.shishkin@gmail.com>
2020-10-19 16:01:25 +0300

    64 Build the DB node in the Camel Route

---
+++ app01-file-db-mail/src/test/java/com/artarkatesoft/learncamel/springbootapps/App01FileDbMailApplicationTests.java
@@ -0,0 +1,14 @@
+package com.artarkatesoft.learncamel.springbootapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+//@Disabled
+class App01FileDbMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
