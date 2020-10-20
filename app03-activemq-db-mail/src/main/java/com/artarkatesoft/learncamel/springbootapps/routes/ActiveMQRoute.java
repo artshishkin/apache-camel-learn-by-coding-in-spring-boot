987c2f64196e95385bfc9bbaaaa4245f2d5a37fc
Art <d.art.shishkin@gmail.com>
2020-10-20 15:06:10 +0300

    77_79 Validation, DB, SQL select

--- app03-activemq-db-mail/src/main/java/com/artarkatesoft/learncamel/springbootapps/routes/ActiveMQRoute.java
+++ app03-activemq-db-mail/src/main/java/com/artarkatesoft/learncamel/springbootapps/routes/ActiveMQRoute.java
@@ -2,8 +2,12 @@ package com.artarkatesoft.learncamel.springbootapps.routes;
 
 import com.artarkatesoft.learncamel.springbootapps.alert.MailProcessor;
 import com.artarkatesoft.learncamel.springbootapps.domain.Item;
+import com.artarkatesoft.learncamel.springbootapps.exceptions.DataException;
+import com.artarkatesoft.learncamel.springbootapps.processors.BuildSQLProcessor;
 import lombok.RequiredArgsConstructor;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.bean.validator.BeanValidationException;
 import org.apache.camel.component.gson.GsonDataFormat;
 import org.springframework.stereotype.Component;
 
@@ -11,39 +15,38 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ActiveMQRoute extends RouteBuilder {
 
-//    private final BuildSQLProcessor buildSQLProcessor;
-
+    private final BuildSQLProcessor buildSQLProcessor;
     private final MailProcessor mailProcessor;
 
     @Override
     public void configure() throws Exception {
-//
-        GsonDataFormat itemFormat = new GsonDataFormat(Item.class);
-//
-//        onException(BeanValidationException.class)
-//                .log(LoggingLevel.ERROR, "Error while validating bean ${body}")
-//                .to("{{errorRoute}}")
-////                .handled(true)
-//        ;
-//
-//        onException(Exception.class)
-//                .log(LoggingLevel.ERROR, "Wrong input message ${body}")
-//                .to("{{errorRoute}}")
-//                .process(mailProcessor)
-////                .handled(true)
-//        ;
 
-//        onException(DataException.class)
-//                .to("log:errorInRoute?level=ERROR&showProperties=true")
-//                .maximumRedeliveries(3)
-//                .redeliveryDelay(400)
-//                .backOffMultiplier(2)
-//                .maximumRedeliveryDelay(999)
-//                .retryAttemptedLogLevel(LoggingLevel.ERROR)
-//                .process(mailProcessor)
-//                .to("{{errorRoute}}")
-////                .handled(true)
-//        ;
+        GsonDataFormat itemFormat = new GsonDataFormat(Item.class);
+
+        onException(BeanValidationException.class)
+                .log(LoggingLevel.ERROR, "Error while validating bean ${body}")
+                .to("{{errorRoute}}")
+//                .handled(true)
+        ;
+
+        onException(Exception.class)
+                .log(LoggingLevel.ERROR, "Wrong input message ${body}")
+                .to("{{errorRoute}}")
+                .process(mailProcessor)
+//                .handled(true)
+        ;
+
+        onException(DataException.class)
+                .to("log:errorInRoute?level=ERROR&showProperties=true")
+                .maximumRedeliveries(3)
+                .redeliveryDelay(400)
+                .backOffMultiplier(2)
+                .maximumRedeliveryDelay(999)
+                .retryAttemptedLogLevel(LoggingLevel.ERROR)
+                .process(mailProcessor)
+                .to("{{errorRoute}}")
+//                .handled(true)
+        ;
 
 
         from("{{routeFromUri}}")
@@ -51,7 +54,12 @@ public class ActiveMQRoute extends RouteBuilder {
                 .log("Read message from ActiveMQ: ${body}")
                 .unmarshal(itemFormat)
                 .log("Unmarshalled item is: ${body}")
-                .to("{{routeTo1Uri}}");
+                .to("bean-validator://itemValidator")
+                .process(buildSQLProcessor)
+                .to("{{routeTo1Uri}}")
+                .to("{{selectNode}}")
+                .to("{{routeTo2Uri}}")
+                .log("Read items from DB are ${body}");
     }
 }
 
