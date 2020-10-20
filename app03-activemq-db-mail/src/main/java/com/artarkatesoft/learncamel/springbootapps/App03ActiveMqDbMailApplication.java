470146abbc6187040307c8a63f4de799119220f5
Art <d.art.shishkin@gmail.com>
2020-10-20 11:52:04 +0300

    72 ActiveMQ SetUp

---
+++ app03-activemq-db-mail/src/main/java/com/artarkatesoft/learncamel/springbootapps/App03ActiveMqDbMailApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.learncamel.springbootapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App03ActiveMqDbMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App03ActiveMqDbMailApplication.class, args);
+	}
+
+}
